374547198d3f2c3b5ee55587dbf62d4053d0a65b
Graham Campbell <graham@mineuk.com>
2014-06-21 16:55:03 +0100

    Injection test

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -30,4 +30,9 @@ use GrahamCampbell\TestBench\Traits\ServiceProviderTestCaseTrait;
 class ServiceProviderTest extends AbstractTestCase
 {
     use ServiceProviderTestCaseTrait;
+
+    public function testSecurityIsInjectable()
+    {
+        $this->assertIsInjectable('GrahamCampbell\Security\Security');
+    }
 }
