4285895d477b173f82b28f90edfc054fdf8620c5
Graham Campbell <graham@alt-three.com>
2015-11-08 19:59:05 +0000

    Improved environment specific code

--- src/SecurityServiceProvider.php
+++ src/SecurityServiceProvider.php
@@ -12,7 +12,9 @@
 namespace GrahamCampbell\Security;
 
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 
 /**
  * This is the security service provider class.
@@ -42,9 +44,9 @@ class SecurityServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/security.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
+        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
             $this->publishes([$source => config_path('security.php')]);
-        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+        } elseif ($app instanceof LumenApplication) {
             $app->configure('security');
         }
 
